2a8dc030e79634875aa9546704cf5681a6453bff
D <77586623+dvxth19@users.noreply.github.com>
2021-03-29 17:25:30 +0300

    Add files via upload

---
+++ i will draw a perfect pentagram for u.py
@@ -0,0 +1,50 @@
+import turtle as t
+t.speed(10)
+t.bgcolor('black')
+t.color('red')
+while True:
+	t.circle(200)
+
+	t.left(72)
+	t.fd(380)
+
+	t.left(144)
+	t.fd(380)
+
+	t.left(144)
+	t.fd(380)
+
+	t.left(144)
+	t.fd(380)
+
+	t.left(144)
+	t.fd(380)
+	
+	t.left(72)
+
+
+
+
+
+    
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
